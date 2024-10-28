6d285e17139e9a5495c87d9ae307f655cf66b9ef
Krystian <krystiansa@protonmail.com>
2024-10-28 16:32:57 +0100

    additional flag for pattern and looking for pattern method

--- searcher.py
+++ searcher.py
@@ -1,14 +1,35 @@
 from pathlib import Path 
 import argparse
-def main(extension, directory):
+import re
+
+def lookingForFile(extension, directory):
     for file in Path(directory).rglob('*' + extension):
-        print(file)
+        return file
+
+def lookingForContent(file, pattern=None):    
+    fileToRead = open(file, 'r')
+    content = fileToRead.read()
+
+    if pattern:
+        pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b' + pattern
+    else:
+        pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    
+    matches = re.findall(pattern,content)
+    for match in matches:
+        print(match)
+    fileToRead.close()
+
+def main(extension, directory, pattern=None):
+    file = lookingForFile(extension, directory)
+    lookingForContent(file, pattern)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Example: searcher.py <extension> <directory>')
     parser.add_argument('extension', help='Extension to search for')
     parser.add_argument('directory', help='Directory to search in')
+    parser.add_argument('-p', '--pattern', help='Additional pattern to search for')
     args = parser.parse_args()
 
-    main(args.extension, args.directory)
+    main(args.extension, args.directory, args.pattern)
